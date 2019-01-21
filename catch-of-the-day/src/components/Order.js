acfb6753ee425fb6df5aaae9e49af53b9d91d39b
Nataliya Ioffe <nxioffe@gmail.com>
2019-01-20 21:27:10 -0500

    beginning animations w CSSTransition

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { formatPrice } from "../helpers";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 class Order extends Component {
   renderOrder = key => {
@@ -11,17 +12,31 @@ class Order extends Component {
 
     if (!isAvailable) {
       return (
-        <li key={key}>
-          Sorry, {fish ? fish.name : "fish"} is no longer available;
-        </li>
+        <CSSTransition
+          classNames="order"
+          key={key}
+          timeout={{ enter: 250, exit: 250 }}
+        >
+          <li key={key}>
+            Sorry, {fish ? fish.name : "fish"} is no longer available;
+          </li>
+        </CSSTransition>
       );
     }
     return (
-      <li key={key}>
-        {count} lbs {fish.name}
-        {formatPrice(count * fish.price)}
-        <button onClick={() => this.props.removeFromOrder(key)}>Remove</button>
-      </li>
+      <CSSTransition
+        classNames="order"
+        key={key}
+        timeout={{ enter: 5000, exit: 5000 }}
+      >
+        <li key={key}>
+          {count} lbs {fish.name}
+          {formatPrice(count * fish.price)}
+          <button onClick={() => this.props.removeFromOrder(key)}>
+            Remove
+          </button>
+        </li>
+      </CSSTransition>
     );
   };
 
@@ -40,7 +55,9 @@ class Order extends Component {
     return (
       <div className="order-wrap">
         <h2>Order</h2>
-        <ul className="order">{orderIds.map(this.renderOrder)}</ul>
+        <TransitionGroup component="ul" className="order">
+          {orderIds.map(this.renderOrder)}
+        </TransitionGroup>
         <div className="total">
           Total:
           <strong>{formatPrice(total)}</strong>
